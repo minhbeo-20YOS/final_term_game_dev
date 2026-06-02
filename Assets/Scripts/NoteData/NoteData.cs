452a771023847107798124dc44343df5a6e72ba2
minhbeo-20YOS <minhdc.24it@vku.udn.vn>
2026-06-02 15:57:46 +0700

    Upload project 3D

--- Assets/Scripts/NoteData/NoteData.cs
+++
@@ -1,6 +0,0 @@
-﻿[System.Serializable]
-public class NoteData
-{
-    public float hitTime; // Giây thứ mấy trong bài hát
-    public int laneIndex; // Vị trí nút (0: Trái, 1: Xuống, 2: Lên, 3: Phải)
-}
